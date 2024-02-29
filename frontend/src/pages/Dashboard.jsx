def0960f495a1efb732c0add0700233cc05f1712
Krishna Ranjan <krishna693rahul@gmail.com>
2024-02-29 11:16:29 +0530

    complete sidebar of the ux

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromURL = urlParams.get("tab");
+    if (tabFromURL) {
+      setTab(tabFromURL);
+    }
+  }, [location.search]);
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="min-h-screen flex flex-col md:flex-row ">
+      <div className="w-56">
+        {/* sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile  */}
+      <DashProfile />
+    </div>
+  );
+};
 
 export default Dashboard;
