a88a1edd9b47a06814b324ed46abcd56e059fd56
Krishna Ranjan <krishna693rahul@gmail.com>
2024-03-04 13:16:19 +0530

    add admin functionality to user

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -19,7 +19,12 @@ const userSchema = new mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fillustrations%2Ficon-profile-user-clip-art-7797704%2F&psig=AOvVaw0K66040soSZ-nRv3kC6ogl&ust=1709206382133000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMiyk4z4zYQDFQAAAAAdAAAAABAE",
+      default:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fillustrations%2Ficon-profile-user-clip-art-7797704%2F&psig=AOvVaw0K66040soSZ-nRv3kC6ogl&ust=1709206382133000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMiyk4z4zYQDFQAAAAAdAAAAABAE",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
   },
   { timestamps: true }
